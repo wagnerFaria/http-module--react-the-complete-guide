6657b4333612ddda5b4346de7cdd0fa5197ac004
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-04-03 23:50:17 -0400

    Aula 190 - Redirecionamento

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 class Posts extends Component {
     state = {
@@ -29,7 +29,9 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
+        // this.setState({ selectedPostId: id });
+        // this.props.history.push({ pathname: '/' + id });//funcionaria assim
+        this.props.history.push('/' + id); //funcionaria tb
     }
 
     render() {
@@ -37,12 +39,13 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>
+                    // <Link to={'/' + post.id} key={post.id}>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
+                    // </Link>
                 );
             });
         }
