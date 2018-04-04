67e3dc73bd1158c79b54a61487b5545f68197d01
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-04-03 22:29:21 -0400

    Aula 180 - Usando component Link (JS Routes)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import NewPost from '../Blog/NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -11,13 +12,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={()=><h1>Home</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
