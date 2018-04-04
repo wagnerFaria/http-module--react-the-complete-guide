f48f1b2870b9997b75e2d8b473b33f824159722d
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-04-04 00:42:17 -0400

    Aula 193 - Dynamic nested routes

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,8 +8,16 @@ class FullPost extends Component {
         loadedPost: null
     }
     componentDidMount() {
+        this.loadData();
+    }
+
+    componentDidUpdate() {
+        this.loadData();
+    }
+
+    loadData = () => {
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== +this.props.match.params.id)) {
                 axios.get('/posts/' + this.props.match.params.id).then((response) => {
                     // console.log(response);
                     this.setState({ loadedPost: response.data });
@@ -20,14 +28,14 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.state.loadedPost.id).then(response => {
+        axios.delete('/posts/' + this.props.match.params.id).then(response => {
             console.log(response);
         });
     }
 
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.id) {
             post = <p style={{ textAlign: 'center' }}>Loading...</p>
         }
         if (this.state.loadedPost) {
