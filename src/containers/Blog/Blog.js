c27e3cf97c864a417ae72acb201e9f82d85fc111
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-04-03 23:42:45 -0400

    Aula 190 - Switch, acha primeira rota e para

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import NewPost from '../Blog/NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
@@ -23,9 +23,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={()=><h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
+
             </div>
         );
     }
